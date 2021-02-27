8fcbd9bb0423f82616c5b7be70576dcc54e63abf
UnicornCodeQueen <75204601+UnicornCodeQueen@users.noreply.github.com>
2021-02-27 17:04:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,55 @@
+function setup() {
+    canvas = createCanvas(380, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    object_detector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting objects";
+}
+
+img = "";
+status = "";
+objects = [];
+
+function preload() {
+    img = loadImage("dog_cat.jpg");
+}
+
+function draw() {
+    image(video, 0, 0, 380, 380);
+    
+    if(status != "")
+    {
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i > objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Obect Detected";
+            document.getElementById("number_of_obejcts").innerHTML = "Number of objects detected are : "+ objects.length;
+
+            fill(r, g, b);
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x, objects[i].y);
+            noFill();
+            stroke(r, g, b);
+            rect(objects[i].x, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function modelLoaded() {
+    console.log("Model Loaded!");
+    status = true;
+    objectDetector.detect(video, gotResult);
+}
+
+function gotResult () {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
